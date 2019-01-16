760733af413fd6029d5600ef2cc6f0a08dd8db7e
Luka Zrnic <luka.zrnic@oroundo.com>
2019-01-16 12:11:55 +0100

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
+const { generateMessage } = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,26 +15,20 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected");
 
-  socket.emit("newMessage", {
-    from: "Admin",
-    text: "Welcome to the chat app",
-    createdAt: new Date().getTime()
-  });
+  socket.emit(
+    "newMessage",
+    generateMessage("Admin", "Welcome to the chat app.")
+  );
 
-  socket.broadcast.emit("newMessage", {
-    from: "Admin",
-    text: "New user joined",
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit(
+    "newMessage",
+    generateMessage("Admin", "New user joined.")
+  );
 
   socket.on("createMessage", message => {
     console.log("Create message", message);
     // salje/prikazuje svim konekcijama
-    io.emit("newMessage", {
-      from: message.from, // from client side
-      text: message.text, // from client side
-      createdAt: new Date().getTime()
-    });
+    io.emit("newMessage", generateMessage(message.from, message.text));
 
     // salje/prikazuje svima osim onome tko je poslao
     // socket.broadcast.emit("newMessage", {
